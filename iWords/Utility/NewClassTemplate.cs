a6613460d872e79200f83dd5f6361621240bae89
Xiang <w.flying@163.com>
2019-03-16 20:58:50 +0800

    Create NewClassTemplate.cs

---
+++ iWords/Utility/NewClassTemplate.cs
@@ -0,0 +1,33 @@
+﻿//---------------------------------------------------------------------------------------
+// <copyright file="NewClassTemplate.cs" company="WangXiang">
+//     Company copyright for iTrade
+// </copyright>
+//---------------------------------------------------------------------------------------
+namespace TradeRobot.Utility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// description
+    /// </summary>
+    class NewClassTemplate
+    {
+        #region //-------------------- private field ------------------------------------
+        #endregion
+
+        #region //-------------------- Public property ----------------------------------
+        #endregion
+
+        //---------------------------- 1st level methods --------------------------------
+
+        #region //-------------------- 2nd level methods --------------------------------
+        #endregion
+
+        #region //-------------------- 3rd level methods --------------------------------
+        #endregion
+    }
+}
